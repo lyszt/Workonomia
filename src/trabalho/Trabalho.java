a2e677eb76853218c7c128eaf254529536820a9d
lyszt <kalliddel@gmail.com>
2025-05-20 18:10:57 -0300

    awesome buttons

---
+++ src/trabalho/Trabalho.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabalho;
+import javax.swing.*;
+
+/**
+ *
+ * @author joaoluis
+ */
+public class Trabalho {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JFrame menu = new Menu();
+       menu.setVisible(true);
+    }
+    
+}
